ba132789a71aac1761df7198a0200076a6794c39
jerome budziak <jerome.budziak@gmail.com>
2013-12-11 16:42:33 +0100

    actionBar : setting

--- src/com/ejp/DivingApp/MyActivity.java
+++ src/com/ejp/DivingApp/MyActivity.java
@@ -2,6 +2,10 @@ package com.ejp.DivingApp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.app.ActionBar;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +15,25 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Settings
+            case R.id.action_settings:
+                //openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
